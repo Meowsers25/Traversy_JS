5efaabab5fa1a6d47e281d84992f651da09e9b5b
Chris <c.lamothe25@gmail.com>
2018-04-03 22:56:23 -0400

    Initial commit

---
+++ JS_sandbox/app.js
@@ -0,0 +1,68 @@
+// alert('Hello World');
+
+// document.querySelector('h1').style.color="red";
+
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = "Hi";
+// console.log(greeting);
+// console.log([1, 2, 3, 4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+// console.error('This is an error');
+// console.clear();
+// console.warn('This is a warning');
+// console.time('Hello');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+// console.timeEnd('Hello');
+
+// var name = 'Jon Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// //init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// //letters, numbers, _, $
+// //Can't start with a number
+// // multi word vars use camelCase
+// var firsrName;
+
+//let
+// let name = 'Jon Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+//const
+const name = 'John';
+console.log(name);
+//cant reassign
+// name = 'Sara';
+
+//have to assign a value
+// const greeting;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+// console.log(person);
+person.name = 'Sara';
+person.age = 32;
+// console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
