b2e678f323bc043546047e05e483bba036da1934
Chris <c.lamothe25@gmail.com>
2018-04-07 22:14:07 -0400

    if statements and conditional operators

--- JS_sandbox/app.js
+++ JS_sandbox/app.js
@@ -351,3 +351,98 @@
 // birthday.setSeconds(25);
 
 // console.log(birthday);
+
+// if(something) {
+// 	do something;
+// } else {
+// 	do something else;
+// }
+
+const id = 100;
+
+//equal to
+// if (id == 100){
+// 	console.log('correct');
+// } else {
+// 	console.log('incorrect');
+// }
+
+//not equal to
+// if (id != 101){
+// 	console.log('correct');
+// } else {
+// 	console.log('incorrect');
+// }
+
+// //equal to value and type
+// if (id === 100){
+// 	console.log('correct');
+// } else {
+// 	console.log('incorrect');
+// }
+
+// //not equal to value and type
+// if (id !== 100){
+// 	console.log('correct');
+// } else {
+// 	console.log('incorrect');
+// }
+
+// if(id){
+// 	console.log(`The ID is ${id}`);
+// } else {
+// 	console.log('No ID');
+//}
+
+//test if undefined
+// if(typeof id !== 'undefined') {
+// 	console.log(`The ID is ${id}`);
+// } else {
+// 	console.log('No ID');
+// }
+
+//greater or less than
+// if(id <= 100) {
+// 	console.log('correct');
+// } else {
+// 	console.log('incorrect');
+// }
+
+//if else
+// const color = 'yellow';
+// if(color === 'red'){
+// 	console.log('Color is red');
+// }else if (color === 'blue'){
+// 	console.log('Color is blue');
+// }else{
+// 	console.log('Color is not red or blue');
+// }
+
+//logical operators
+const name = 'Steve';
+const age = 17;
+
+//and &&
+if(age > 0 && age < 12){
+	console.log(`${name} is a child`);
+}else if(age >= 13 && age <= 19){
+	console.log(`${name} is a teenager`);
+}else{
+	console.log(`${name} is an adult`);
+}
+
+//or ||
+if(age < 16 || age > 65){
+	console.log(`${name} can not enter the race`);
+}else{
+	console.log(`${name} can enter the race`);
+}
+
+//ternary operator
+console.log(id === 100 ? 'correct' : 'incorrect');
+
+//without curly braces
+if(id === 100)
+	console.log('correct');
+else
+	console.log('incorrect');
